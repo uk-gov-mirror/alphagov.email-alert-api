78bf4c6222eee99615e03339d6d469db9b252e9d
Kelvin Gan <kelvin.gan@digital.cabinet-office.gov.uk>
2020-04-30 14:56:56 +0100

    Update SubscriberListMove to send emails
    The SubscriberListMove can now optionally send an email to the
    subscribers to let them know of being moved.
    
    This change is to support moving subscribers from a list based on
    the "coronavirus-covid-19-uk-government-response" topical event
    to a list based on the "coronavirus-taxon" taxon and to let them
    know.

--- lib/subscriber_list_mover.rb
+++ lib/subscriber_list_mover.rb
@@ -1,7 +1,8 @@
 class SubscriberListMover
-  attr_reader :from_slug, :to_slug
+  attr_reader :from_slug, :to_slug, :send_email
 
-  def initialize(from_slug:, to_slug:)
+  def initialize(from_slug:, to_slug:, send_email: false)
+	  @send_email = send_email
     @from_slug = from_slug
     @to_slug = to_slug
   end
@@ -36,6 +37,8 @@ class SubscriberListMover
         )
 
         if subscribed_to_destination_subscriber_list.nil?
+          puts "Moving #{subscriber.address} with ID #{subscriber.id} to #{destination_subscriber_list.title} list"
+
           Subscription.create!(
             subscriber: subscriber,
             subscriber_list: destination_subscriber_list,
@@ -47,5 +50,38 @@ class SubscriberListMover
     end
 
     puts "#{subscribers.count} active subscribers moved from #{from_slug} to #{to_slug}"
+
+    if send_email
+      email_change_to_subscribers(source_subscriber_list)
+    end
+  end
+
+  def email_change_to_subscribers(source_subscriber_list)
+    email_subject = "Changes to GOV.UK email alerts"
+    email_utm_parameters = {
+        utm_source: from_slug,
+        utm_medium: "email",
+        utm_campaign: "govuk-subscription-ended",
+    }
+    email_redirect = EmailTemplateContext.new.add_utm("https://gov.uk/email/manage", email_utm_parameters)
+    list_title = source_subscriber_list.title
+
+    bulk_move_template = <<~BODY.freeze
+      Hello,
+
+      You've subscribed to get email alerts about #{list_title} information.
+
+      GOV.UK is changing the type of email alerts you get to make sure you find out when any #{list_title} guidance is added or updated.
+      You can [change how often you get updates or unsubscribe](#{email_redirect}) from GOV.UK email alerts.
+
+      Thanks,
+      GOV.UK
+    BODY
+
+    BulkEmailSenderService.call(
+      subject: email_subject,
+      body: bulk_move_template,
+      subscriber_lists: source_subscriber_list,
+    )
   end
 end
