d72daaef343e89b59d4165617e40c5f080fc8d94
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-10-23 09:02:43 +0100

    WIP

--- config/initializers/gds_sso.rb
+++ config/initializers/gds_sso.rb
@@ -2,7 +2,7 @@ GDS::SSO.config do |config|
   config.user_model = "User"
   config.oauth_id = ENV.fetch("OAUTH_ID", "abcdefg")
   config.oauth_secret = ENV.fetch("OAUTH_SECRET", "secret")
-  config.oauth_root_url = Plek.find("signon")
+  config.oauth_root_url = Plek.new.external_url_for("signon")
   config.cache = Rails.cache
   config.additional_mock_permissions_required = %w[internal_app]
 end
